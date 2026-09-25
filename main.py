2bb85705d00dda6c477ef1e953060f4798f5fe78
onwabelungani4-byte <onwabelungani4@gmail.com>
2026-09-25 15:56:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from flask import Flask
+from threading import Thread
+app = Flask('')
+@app.route('/')
+def home(): return "Bot Online"
+def run(): app.run(host='0.0.0.0', port=8080)
+Thread(target=run).start()
+
+
+
+
 import discord, os
 from discord.ext import commands
 
